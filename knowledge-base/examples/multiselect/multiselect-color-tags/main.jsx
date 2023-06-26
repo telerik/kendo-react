5ed227513898cff6c512281fbbce863d03c75169
filipKovachev <kovachev@progress.com>
2023-06-26 23:26:45 +0300

    remove double scroll article, add multiselect one

---
+++ knowledge-base/examples/multiselect/multiselect-color-tags/main.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { MultiSelect } from '@progress/kendo-react-dropdowns';
+
+const sizes = [
+  'X-Small - Event',
+  'Small - Visit',
+  'Medium - Visit',
+  'Large - Event',
+  'X-Large - Visit',
+  '2X-Large - Event',
+];
+
+const App = () => {
+  const [value, setValue] = React.useState([]);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const tagRender = (tagData, li) => {
+    const tagText = tagData.data[0];
+    let backgroundColor = '';
+
+    if (tagText.includes('Event')) {
+      backgroundColor = 'red';
+    } else if (tagText.includes('Visit')) {
+      backgroundColor = 'yellow';
+    }
+
+    const tagStyle = {
+      background: backgroundColor,
+      display: 'inline-block',
+      padding: '2px 8px',
+      borderRadius: '4px',
+      color: '#fff',
+    };
+
+    return React.cloneElement(
+      li,
+      {
+        ...li.props,
+        style: {
+          ...li.props.style,
+          backgroundColor: backgroundColor,
+        },
+      },
+      [
+        <span key={tagText} className="custom-tag" style={tagStyle}>
+          {tagText}
+        </span>,
+        li.props.children,
+      ]
+    );
+  };
+  return (
+    <div>
+      <div>T-shirt sizes:</div>
+      <MultiSelect
+        style={{ width: '300px' }}
+        data={sizes}
+        value={value}
+        onChange={handleChange}
+        tagRender={tagRender}
+        placeholder="Please select ..."
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('my-app'));
