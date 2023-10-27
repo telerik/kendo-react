4eff94c7d76735d4cbfa21d29c2fbdd747faddc2
Wissam <merhi@progress.com>
2023-10-27 18:25:32 +0300

    docs(grid): disable column reordering KB

---
+++ knowledge-base/examples/grid/grid-disable-reodering/main.jsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import { Button } from '@progress/kendo-react-buttons';
+import products from './products.json';
+
+const App = () => {
+  const [locked, setLocked] = React.useState(false);
+
+  const handleClick = () => {
+    setLocked(!locked);
+  };
+
+  const onColumnReorder = (ev) => {
+    // debugger;
+  };
+  return (
+    <div>
+      <div className="mb-3">
+        <div className="mb-2">
+          <Button themeColor={'primary'} onClick={handleClick}>
+            Lock / Unlock Additional details Column
+          </Button>
+        </div>
+
+        <div
+          className={
+            'w-50 alert ' + (locked ? 'alert-success' : 'alert-danger')
+          }
+        >
+          The Additional details Column is
+          <strong className="text-muted">
+            {locked ? ' Frozen' : ' Unfrozen'}
+          </strong>
+        </div>
+      </div>
+      <Grid
+        style={{
+          height: 350,
+        }}
+        data={products}
+        reorderable={true}
+        onColumnReorder={onColumnReorder}
+      >
+        <Column
+          field="ProductID"
+          title="Id"
+          width="45px"
+          locked={true}
+          orderIndex={0}
+        />
+        <Column
+          field="UnitsInStock"
+          title="Units"
+          width="90px"
+          locked={true}
+          orderIndex={0}
+        />
+        <Column
+          field="Category.CategoryName"
+          title="CategoryName"
+          width="250px"
+        />
+        <Column field="UnitPrice" title="Price" width="90px" />
+
+        <Column field="UnitsOnOrder" title="On order" width="90px" />
+        <Column field="Discontinued" width="120px" />
+        <Column
+          field="QuantityPerUnit"
+          title="Additional details"
+          width="250px"
+          locked={locked}
+        />
+      </Grid>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('my-app'));
