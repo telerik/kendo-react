f9b2dab95511425fde4fbb9af0270e5ed1376620
filipKovachev <kovachev@progress.com>
2024-01-25 13:17:54 +0200

    docs(editor): add maxwidth kb

---
+++ knowledge-base/examples/editor/set-maxwidth/main.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import content from './content';
+import { Editor, EditorTools } from '@progress/kendo-react-editor';
+
+const { Bold, Italic, Underline } = EditorTools;
+
+const styles = `
+    html {
+      background-color: darkBlue;
+    }
+    body {
+        background-color: #53d2fa;
+        margin: 30px;
+        padding-left: 50px;
+        max-width: 650px;
+    }
+    .text-cursor {
+      cursor: text;
+    }
+`;
+
+const App = () => {
+
+  const onMount = (event) => {
+    const iframeDocument = event.dom.ownerDocument;
+    const style = iframeDocument.createElement('style');
+    style.appendChild(iframeDocument.createTextNode(styles));
+    iframeDocument.head.appendChild(style);
+
+    const htmlElement = iframeDocument.documentElement;
+    htmlElement.addEventListener('click', () => {
+      const contentEditableElement = iframeDocument.querySelector(
+        '[contenteditable="true"]'
+      );
+      if (contentEditableElement) {
+        contentEditableElement.focus();
+      }
+    });
+  };
+
+  return (
+    <Editor
+      tools={[[Bold, Italic, Underline]]}
+      defaultContent={content}
+      onMount={onMount}
+    />
+  );
+};
+ReactDOM.render(<App />, document.querySelector('my-app'));
