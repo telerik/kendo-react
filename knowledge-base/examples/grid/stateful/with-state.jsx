a7711de0c3f6d53b7ec55de36ef8f703c0246474
filipKovachev <kovachev@progress.com>
2024-02-28 17:50:53 +0200

    docs(grid): refactor to functional component

--- knowledge-base/examples/grid/stateful/with-state.jsx
+++ knowledge-base/examples/grid/stateful/with-state.jsx
@@ -1,33 +1,35 @@
-import * as React from 'react';
+import React, { useState } from "react";
+import { process } from "@progress/kendo-data-query";
 
-import { process } from '@progress/kendo-data-query';
+export const withState = (WrappedGrid) => {
+  return (props) => {
+    const [gridState, setGridState] = useState(
+      props.pageable === false
+        ? {}
+        : { skip: 0, take: 10, sort: [], filter: null }
+    );
 
-export function withState(WrappedGrid) {
-    return class StatefulGrid extends React.Component {
-        constructor(props) {
-            super(props);
-            if (props.pageable === false) {
-                this.state = {};
-            } else {
-                this.state = { skip: 0, take: 10 };
-            }
-        }
-
-        render() {
-            return (
-                <WrappedGrid
-                    filterable={true}
-                    sortable={true}
-                    pageable={{ pageSizes: true }}
-
-                    {...this.props}
-                    {...this.state}
-
-                    data={process(this.props.data, this.state)}
-
-                    onDataStateChange={(e) => { this.setState(e.data); }}
-                />
-            );
-        }
+    const onDataStateChange = (event) => {
+      const updatedState = {
+        ...gridState,
+        skip: event.dataState?.skip,
+        take: event.dataState?.take,
+        sort: event.dataState?.sort,
+        filter: event.dataState?.filter,
+      };
+      setGridState(updatedState);
     };
-}
+
+    return (
+      <WrappedGrid
+        filterable={true}
+        sortable={true}
+        pageable={{ pageSizes: true }}
+        {...props}
+        data={process(props.data, gridState)}
+        onDataStateChange={onDataStateChange}
+        {...gridState}
+      />
+    );
+  };
+};
