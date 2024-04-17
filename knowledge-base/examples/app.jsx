8a2488c346aeaf99e4cf54c3a464d2dbf4f82110
Boryana Todorova <botodoro@progress.com>
2024-04-17 14:37:06 +0300

    chore: change app name

--- knowledge-base/examples/app.jsx
+++ knowledge-base/examples/app.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './editor/attach-events/main.jsx'
+import App from './editor/attach-events/app.jsx'
 
 const root = createRoot(document.querySelector('my-app'));
 root.render(<App />);
