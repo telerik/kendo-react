5009ba9154118257f8193f608b7e727457324d8e
filipKovachev <kovachev@progress.com>
2024-05-06 20:43:41 +0300

    add multilevel list styles KB article

---
+++ knowledge-base/editor-multilevel-list-styles.md
@@ -0,0 +1,38 @@
+---
+title: Add different bullet styles to multilevel lists for the Editor
+description: Learn how to add different bullet styles to multilevel lists for the Editor
+type: how-to
+page_title: Add different bullet styles to multilevel lists  - KendoReact Editor
+slug: editor-multilevel-list-styles
+tags: editor, kendoreact, bullet, multilevel, styles
+ticketid: 1649134
+res_type: kb
+category: knowledge-base
+---
+
+## Environment
+
+<table>
+	<tbody>
+		<tr>
+			<td>Product Version</td>
+			<td>7.4.0</td>
+		</tr>
+		<tr>
+			<td>Product</td>
+			<td>Progress® KendoReact</td>
+		</tr>
+	</tbody>
+</table>
+
+## Description
+
+How can I apply different bullet styles to different indentation levels in the KendoReact Editor? 
+
+## Solution
+
+This can be achieved by adding a [style element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/style) to the iframe document that contains the required [list-style-type](https://www.w3schools.com/cssref/pr_list-style-type.php)
+
+{% meta id:index height:330 %}
+{% embed_file editor/editor-multilevel-list-styles/main.jsx preview %}
+{% endmeta %}
---
+++ knowledge-base/examples/editor/editor-multilevel-list-styles/main.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Editor, EditorTools } from '@progress/kendo-react-editor';
+
+const {
+  Bold,
+  Italic,
+  Underline,
+  Undo,
+  Redo,
+  Indent,
+  Outdent,
+  OrderedList,
+  UnorderedList,
+} = EditorTools;
+
+const styles = `
+ol {
+  list-style-type: decimal; 
+}
+
+ol li {
+  position: relative;
+  padding-left: 30px; 
+}
+
+ol li p {
+  margin-left: 1em;
+}
+
+ol li ol {
+  list-style-type: lower-roman; 
+}
+
+ol li ol li ol {
+  list-style-type: upper-roman; 
+}
+
+ol li ol li ol li ol {
+  list-style-type: lower-alpha; 
+}
+
+ol li ol li ol li ol li ol {
+  list-style-type: upper-alpha; 
+}
+
+ol li ol li ol li ol li ol li ol {
+  list-style-type: none;
+}
+
+ol li ol li ol li ol li ol li ol li p {
+  margin-left: 4.1em;
+}
+`;
+
+const content = `
+<ol>
+   <li>
+      <p>Text formatting</p>
+   </li>
+   <li>
+      <p>Cross-browser support</p>
+      <ol>
+         <li>
+            <p>Text formatting</p>
+            <ol>
+               <li>
+                  <p>Bulleted and numbered lists</p>
+                  <ol>
+                     <li>
+                        <p>Test 1</p>
+                        <ol>
+                           <li>
+                              <p>Test 2</p>
+                           </li>
+                        </ol>
+                     </li>
+                  </ol>
+               </li>
+            </ol>
+         </li>
+      </ol>
+   </li>
+   <li>
+      <p>Identical HTML output across browsers</p>
+   </li>
+</ol>`;
+
+const App = () => {
+  const onMount = (event) => {
+    const iframeDocument = event.dom.ownerDocument;
+    const style = iframeDocument.createElement('style');
+    style.appendChild(iframeDocument.createTextNode(styles));
+    iframeDocument.head.appendChild(style);
+  };
+
+  return (
+    <Editor
+      tools={[
+        [Bold, Italic, Underline],
+        [OrderedList, UnorderedList, Indent, Outdent],
+        [Undo, Redo],
+      ]}
+      defaultContent={content}
+      onMount={onMount}
+    />
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('my-app'));
