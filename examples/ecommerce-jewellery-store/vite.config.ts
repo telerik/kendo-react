c07ffad2d35ddb8851a369c4cab04366afae50ee
Boryana Todorova <botodoro@progress.com>
2024-11-15 22:31:18 +0200

    chore: add base path

--- examples/ecommerce-jewellery-store/vite.config.ts
+++ examples/ecommerce-jewellery-store/vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/kendo-react/ecommerce-jewellery-store/`,
   plugins: [react()],
   resolve: {
     alias: {
