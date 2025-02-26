d8335a4384af52b074cf18f3b0f5dbf943c1df41
Silviya Boteva <sboteva@progress.com>
2025-02-26 23:40:07 +0200

    chore: add projects page

--- examples/kendo-react-freemium/src/pages/Projects.tsx
+++ examples/kendo-react-freemium/src/pages/Projects.tsx
@@ -1,7 +1,136 @@
+import { Avatar, Breadcrumb } from "@progress/kendo-react-layout";
+import { Button } from "@progress/kendo-react-buttons";
+import { Grid, GridColumn, GridCustomCellProps, GridToolbar } from "@progress/kendo-react-grid";
+import { exportIcon, homeIcon, plusIcon, printIcon, searchIcon } from "@progress/kendo-svg-icons";
+import { SvgIcon } from "@progress/kendo-react-common";
+import { InputPrefix, TextBox } from "@progress/kendo-react-inputs";
+import { Badge } from "@progress/kendo-react-indicators";
+import { process } from "@progress/kendo-data-query";
+import React from "react";
+import { projectsData } from "./data";
+
+interface DataModel {
+  id: string;
+  text?: string;
+  icon?: React.ReactNode;
+  iconClass?: string;
+}
+
+const breadcrumbItems: DataModel[] = [
+  {
+      id: "home",
+      icon: <SvgIcon icon={homeIcon} />,
+  },
+  {
+      id: "projects",
+      text: "Projects"
+  },
+  // {
+  //     id: "project1",
+  //     text: "Online Learning Management System (LMS)"
+  // }
+];
+
+const ProjectManagerCell = (props: GridCustomCellProps) => {
+  return <td {...props.tdProps}>
+    <div className="flex gap-1 items-center">
+      <Avatar type="image" rounded="full" size="small">
+        <img src={props.dataItem.AvatarSrc} alt="user-image" />
+      </Avatar>
+      <span>{props.dataItem.ProjectManager}</span>
+    </div>
+  </td>;
+};
+
+const StakeholderCell = (props: GridCustomCellProps) => {
+  return <td {...props.tdProps}>
+    <div className="flex gap-1 items-center">
+      <Avatar type="image" rounded="full" size="small">
+        <img src={props.dataItem.AvatarSrc} alt="user-image" />
+      </Avatar>
+      <span>{props.dataItem.Stakeholders}</span>
+    </div>
+  </td>;
+};
+
+const PriorityCell = (props: GridCustomCellProps) => {
+  return <td {...props.tdProps}>
+    <Badge rounded="full" position="inside" className="!relative" themeColor={
+      props.dataItem.Priority === "Urgent" ? "error"
+        : props.dataItem.Priority === "Medium priority" ? "warning"
+          : props.dataItem.Priority === "Low priority" ? "success"
+            : props.dataItem.Priority === "Routine" ? "info" : "primary"}>
+      {props.dataItem.Priority}
+    </Badge>
+  </td>;
+};
+
+const initialDataState = {
+  skip: 0,
+  take: 10
+};
+
 export default function Projects() {
+  const [page, setPage] = React.useState(initialDataState);
+  const [pageSizeValue, setPageSizeValue] = React.useState();
+  const data = process(projectsData, {
+    skip: page.skip,
+    take: page.take
+  });
+  const pageChange = (event: any) => {
+    const targetEvent = event.targetEvent;
+    const take = targetEvent.value === 'All' ? projectsData.length : event.page.take;
+    if (targetEvent.value) {
+      setPageSizeValue(targetEvent.value);
+    }
+    setPage({
+      ...event.page,
+      take
+    });
+  };
+
   return (
-    <>
-      Projects
-    </>
+    <div style={{minHeight: 'calc(100vh - 106px)'}} className="flex flex-col p-10 gap-6">
+      <Breadcrumb data={breadcrumbItems} className="!bg-app-surface" />
+
+      <div className="flex flex-wrap items-center justify-between">
+          <h1 className="!text-4xl">Projects</h1>
+          <Button themeColor="primary" fillMode="outline" svgIcon={plusIcon}>Add new project</Button>
+      </div>
+
+      <Grid className="k-grid-no-scrollbar"
+          data={data}
+          skip={page.skip}
+          take={page.take}
+          total={projectsData.length}
+          pageable={{
+              buttonCount: 6,
+              pageSizes: true,
+              pageSizeValue: pageSizeValue
+            }} onPageChange={pageChange}
+      >
+        <GridToolbar>
+            <TextBox
+              // value={filterValue}
+            // onChange={onFilterChange}
+              className="k-grid-search"
+              placeholder="Search..."
+              prefix={() => (
+                <InputPrefix>
+                  <SvgIcon icon={searchIcon} />
+                </InputPrefix>
+              )}
+            />
+            <span className="k-spacer"></span>
+            <Button svgIcon={exportIcon} fillMode="flat">Export to CSV</Button>
+            <Button svgIcon={printIcon} fillMode="flat">Print</Button>
+        </GridToolbar>
+        <GridColumn field="ProjectName" title="Project Name" />
+        <GridColumn field="ProjectManager" title="Project Manager" width={245} cells={{ data: ProjectManagerCell }} />
+        <GridColumn field="Stakeholders" title="Stakeholder(s)" width={245} cells={{ data: StakeholderCell }} />
+        <GridColumn field="DueDate" title="Due Date" width={245} />
+        <GridColumn field="Priority" title="Priority" width={225} cells={{ data: PriorityCell }}/>
+      </Grid>
+    </div>
   )
 }
---
+++ examples/kendo-react-freemium/src/pages/data.ts
@@ -0,0 +1,522 @@
+
+import Avatar0 from '../assets/Avatar.png';
+import Avatar1 from '../assets/Avatar-1.png';
+import Avatar2 from '../assets/Avatar-2.png';
+import Avatar3 from '../assets/Avatar-3.png';
+import Avatar4 from '../assets/Avatar-4.png';
+import Avatar5 from '../assets/Avatar-5.png';
+import Avatar6 from '../assets/Avatar-6.png';
+import Avatar7 from '../assets/Avatar-7.png';
+import Avatar8 from '../assets/Avatar-8.png';
+import Avatar9 from '../assets/Avatar-9.png';
+
+export const listData = [
+  { text: "Review LMS project timelines and milestones." },
+  { text: "Research new investment possibilities for your portfolio." },
+  { text: "Communicate with stakeholders on project X updates." },
+  { text: "Analyze costs and make recommendations for cost-saving initiatives." },
+  { text: "Attend today’s client meeting to discuss the project X requirements." },
+  { text: "Review current workflows for project X and identify areas for improvement." },
+  { text: "Review LMS project timelines and milestones." },
+  { text: "Research new investment possibilities for your portfolio." },
+  { text: "Communicate with stakeholders on project X updates." },
+  { text: "Analyze costs and make recommendations for cost-saving initiatives." },
+  { text: "Attend today’s client meeting to discuss the project X requirements." },
+  { text: "Review current workflows for project X and identify areas for improvement." }
+];
+
+export const gridData = [
+  { taskName: "#2213 Define project scope and objectives.", status: "🕒 Postponed" },
+  { taskName: "#2214 Conduct market research to assess existing LMS solutions.", status: "✅ Completed" },
+  { taskName: "#5342 Identify target audience and their specific needs.", status: "⚠ Delayed" },
+  { taskName: "#7211 Create a project timeline with milestones and deliverables.", status: "⏳ In Progress" },
+  { taskName: "#2811 Gather requirements from stakeholders through interviews or surveys.", status: "❌ Cancelled" },
+  { taskName: "#2213 Define project scope and objectives.", status: "🕒 Postponed" },
+  { taskName: "#2214 Conduct market research to assess existing LMS solutions.", status: "✅ Completed" },
+  { taskName: "#5342 Identify target audience and their specific needs.", status: "⚠ Delayed" },
+  { taskName: "#7211 Create a project timeline with milestones and deliverables.", status: "⏳ In Progress" },
+  { taskName: "#2811 Gather requirements from stakeholders through interviews or surveys.", status: "❌ Cancelled" },
+];
+
+export const projectsData = [
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Maegan_Dietrich56",
+    Stakeholders: "Judy.Beatty",
+    DueDate: "21/08/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar1
+  },
+  {
+    ProjectName: "Market Research and Analytics Tool",
+    ProjectManager: "Howell69",
+    Stakeholders: "Mary.Flatley37",
+    DueDate: "17/07/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar2
+  },
+  {
+    ProjectName: "Virtual Reality Training Module for Employee Onboarding",
+    ProjectManager: "Kory.Lockman",
+    Stakeholders: "Lynn_Kunde",
+    DueDate: "24/05/2025",
+    Priority: "Low priority",
+    AvatarSrc: Avatar3
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Marlon66",
+    Stakeholders: "Monserrat49",
+    DueDate: "08/11/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar0
+  },
+  {
+    ProjectName: "Automated Software Development Tool",
+    ProjectManager: "Nolan.Kling13",
+    Stakeholders: "Gabrielle90",
+    DueDate: "01/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar4
+  },
+  {
+    ProjectName: "Telehealth and Health Tracking Application",
+    ProjectManager: "Antoinette_Hills",
+    Stakeholders: "Lavina.Grady",
+    DueDate: "15/12/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar5
+  },
+  {
+    ProjectName: "Collaborative Agile Project Management Suite",
+    ProjectManager: "Travis.Gerhold80",
+    Stakeholders: "Deborah.Stamm",
+    DueDate: "05/09/2025",
+    Priority: "Routine",
+    AvatarSrc: Avatar7
+  },
+  {
+    ProjectName: "Incident Response Coordination Portal",
+    ProjectManager: "Jamaal41",
+    Stakeholders: "Skylar_Smith65",
+    DueDate: "12/07/2025",
+    Priority: "High priority",
+    AvatarSrc: Avatar8
+  },
+  {
+    ProjectName: "Online Learning Management System (LMS)",
+    ProjectManager: "Alden_Upton",
+    Stakeholders: "Lenna_Schmidt30",
+    DueDate: "03/05/2025",
+    Priority: "Urgent",
+    AvatarSrc: Avatar6
+  },
+  {
+    ProjectName: "AI-Powered Bug Tracking and Resolution System",
+    ProjectManager: "Fabiola.Cassin90",
+    Stakeholders: "Richmond_Bruen",
+    DueDate: "31/07/2025",
+    Priority: "Medium priority",
+    AvatarSrc: Avatar9
+  }
+];
