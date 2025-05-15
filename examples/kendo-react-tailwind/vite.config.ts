bb69a1aa96c95bfb22b7bb7628059c9e3f0fb7db
Boryana Todorova <botodoro@progress.com>
2025-05-15 09:52:18 +0300

    chore: deploy kendo-react-tailwind app to gh-pages

--- examples/kendo-react-tailwind/vite.config.ts
+++ examples/kendo-react-tailwind/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: `/kendo-react/kendo-react-tailwind/`,
     plugins: [react(), tailwindcss()]
 });
