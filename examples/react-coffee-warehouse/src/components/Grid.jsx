f337675b4a9f189bac475d3ec70d6dfe0b02d34f
Silviya Boteva <sboteva@progress.com>
2025-05-30 23:47:03 +0300

    chore: update grid props in react-coffee-warehouse app

--- examples/react-coffee-warehouse/src/components/Grid.jsx
+++ examples/react-coffee-warehouse/src/components/Grid.jsx
@@ -6,10 +6,10 @@ import { Grid as KendoGrid, GridColumn, GridColumnMenuSort, GridColumnMenuFilter
 import { Button } from '@progress/kendo-react-buttons'
 import { GridPDFExport } from '@progress/kendo-react-pdf';
 import { ExcelExport } from '@progress/kendo-react-excel-export';
-import { process } from '@progress/kendo-data-query';
 import { Input } from '@progress/kendo-react-inputs';
 import { useLocalization } from '@progress/kendo-react-intl';
 import { fileExcelIcon, filePdfIcon } from '@progress/kendo-svg-icons';
+import { filterBy } from '@progress/kendo-data-query';
 
 export const Column = GridColumn;
 
@@ -23,39 +23,17 @@ export const ColumnMenu = (props) => {
 }
 
 export const Grid = (props) => {
-    const { data, onDataChange, ...others } = props;
+    const { data, onDataChange, defaultFilter, ...others } = props;
 
     const excelExportRef = React.useRef(null);
     const pdfExportRef = React.useRef(null);
 
     const [isPdfExporting, setIsPdfExporting] = React.useState(false);
-    const [take, setTake] = React.useState(10);
-    const [skip, setSkip] = React.useState(0);
-    const [sort, setSort] = React.useState([]);
-    const [group, setGroup] = React.useState([]);
-    const [filter, setFilter] = React.useState(null);
-    const lastSelectedIndexRef = React.useRef(0);
-    const [allColumnFilter, setAllColumnFilter] = React.useState('');
+    const [filter, setFilter] = React.useState(defaultFilter);
+    const filteredData = filterBy(data, filter);
+    const [select, setSelect] = React.useState({});
     const localizationService = useLocalization();
 
-    const dataState = {
-        take,
-        skip,
-        sort,
-        group,
-        filter
-    };
-
-    const onDataStateChange = React.useCallback(
-        (event) => {
-            setTake(event.dataState.take);
-            setSkip(event.dataState.skip);
-            setSort(event.dataState.sort);
-            setGroup(event.dataState.group);
-            setFilter(event.dataState.filter);
-        },
-        [setTake, setSkip, setSort, setGroup]
-    );
 
     const onExcelExport = React.useCallback(
         () => {
@@ -73,122 +51,68 @@ export const Grid = (props) => {
         []
     );
 
-    const onAllColumnFilterChange = React.useCallback(
+    const handleFilterChange = React.useCallback(
         (event) => {
-            setAllColumnFilter(event.value);
+            let filter = {
+                logic: 'or',
+                filters: [
+                    { field: 'contactName', operator: 'contains', value: event.value },
+                    { field: 'jobTitle', operator: 'contains', value: event.value },
+                    { field: 'budget', operator: 'contains', value: event.value },
+                    { field: 'phone', operator: 'contains', value: event.value },
+                    { field: 'address', operator: 'contains', value: event.value }
+                ]
+            };
+            setFilter(filter);
         },
-        [setAllColumnFilter]
+        [setFilter]
     );
 
-    const onSelectionChange = React.useCallback(
-        (event) => {
-            let last = lastSelectedIndexRef.current;
-            const updatedData = data.map(dataItem => {
-                return {...dataItem};
-            });
-            const current = data.findIndex(dataItem => dataItem === event.dataItem);
+    const onSelectionChange = React.useCallback((event) => {
+        setSelect(event.select);
+    }, []);
 
-            if (!event.nativeEvent.shiftKey) {
-                lastSelectedIndexRef.current = last = current;
-            }
-
-            if (!event.nativeEvent.ctrlKey) {
-                updatedData.forEach(item => (item.selected = false));
-            }
-            const select = !event.dataItem.selected;
-            for (let i = Math.min(last, current); i <= Math.max(last, current); i++) {
-                updatedData[i].selected = select;
-            }
-
-            onDataChange(updatedData);
-        },
-        [data, onDataChange]
-    );
-
-    const onHeaderSelectionChange = React.useCallback(
-        (event) => {
-            const checked = event.syntheticEvent.target.checked;
-            const updatedData = data.map(item=>{
-                return {
-                    ...item,
-                    selected: checked
-                };
-            });
-
-            onDataChange(updatedData);
-        },
-        [data, onDataChange]
-    );
-
-    const textColumns = props.children.map(col => {
-            if (col.props.children) {
-                return col.props.children.map(child => {
-                    if (!child.props.filter || child.props.filter === "text") {
-                        return child.props.field;
-                    }
-                    return null;
-                });
-            } else if (col.props.field) {
-                if (!col.props.filter || col.props.filter === "text") {
-                    return col.props.field;
-                }
-            }
-            return null;
-        })
-        .flat()
-        .filter(field => field);
-
-    const allColumnsFilters = textColumns.map(column => ({
-        field: column,
-        operator: 'contains',
-        value: allColumnFilter
-    }));
-
-    const allColumnFilteredData = allColumnFilter ?
-                                    process(data, {filter: { logic: "or", filters: allColumnsFilters }}).data :
-                                    data;
-
-    const processedData = process(allColumnFilteredData, dataState);
-
-    React.useEffect(
-        () => {
-            if (!processedData.data.length) {
-                setSkip(0);
-            }
-        },
-        [processedData]
-    );
+    const onHeaderSelectionChange = React.useCallback((event) => {
+        setSelect(event.select);
+    }, []);
 
     const onPdfExport = React.useCallback(
         () => {
             if (pdfExportRef.current) {
                 setIsPdfExporting(true);
-                pdfExportRef.current.save(processedData.data, onPdfExportDone);
+                pdfExportRef.current.save(data, onPdfExportDone);
             }
         },
-        [processedData, onPdfExportDone]
+        [data, onPdfExportDone]
     );
 
     const GridElement = (
         <KendoGrid
-            {...dataState}
             {...others}
             rowHeight={40}
             pageable
+            defaultSkip={0}
+            defaultTake={10}
             sortable
             groupable
-            selectedField={'selected'}
+            filter={filter}
 
-            data={processedData}
-            onDataStateChange={onDataStateChange}
-
+            data={filteredData}
+            dataItemKey={'id'}
+            autoProcessData={true}
+            selectable={{
+                enabled: true,
+                drag: false,
+                cell: false,
+                mode: 'single'
+            }}
+            select={select}
             onSelectionChange={onSelectionChange}
             onHeaderSelectionChange={onHeaderSelectionChange}
         >
             <GridToolbar>
                 <Input
-                    value={allColumnFilter}
-                    onChange={onAllColumnFilterChange}
+                    onChange={handleFilterChange}
                     placeholder={localizationService.toLanguageString('custom.gridSearch')}
                 />
                 <Button
@@ -205,14 +129,7 @@ export const Grid = (props) => {
                     {localizationService.toLanguageString('custom.exportPdf')}
                 </Button>
             </GridToolbar>
-            <Column
-                field={'selected'}
-                width={50}
-                title={' '}
-                headerSelectionValue={
-                    data.findIndex(dataItem => dataItem.selected === false) === -1
-                }
-            />
+            <Column columnType="checkbox" />
             {props.children}
         </KendoGrid>
     );
