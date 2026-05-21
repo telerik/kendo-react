6b32b61f8f3201b6a264c1b45ff36b3a84eec8dd
Petar Todorov <41293735+PekoPPT@users.noreply.github.com>
2026-05-21 16:49:06 +0300

    chore: refactor file manager app to work on vite

---
+++ examples/kendo-react-file-manager/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(<App />);
